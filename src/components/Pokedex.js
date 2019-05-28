86b6ca52a997f81637e76a8657e4ea520657bfc3
Matt Basile <matthewbasile2@gmail.com>
2019-05-28 18:10:00 -0400

    updating stlyes with Tailwind reboot

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -14,15 +14,15 @@ export default class Pokedex extends Component {
         
 
         <div className="pokedex-container">
-          <div className="left-pokedex-container bg-red-light">
+          <div className="left-pokedex-container bg-red-500">
             <div className="poke-light-container">
               <div className="box-1">
-                <div className="blue-light bg-blue-light ">
+                <div className="blue-light bg-blue-400 ">
                 </div>
                 <div className="three-lights w-1/2">
-                  <div className="light red-light bg-red w-1/3"></div>
-                  <div className="light yellow-light bg-yellow w-1/3"></div>
-                  <div className="light green-light bg-green w-1/3"></div>
+                  <div className="light red-light bg-red-500 w-1/3"></div>
+                  <div className="light yellow-light bg-yellow-500 w-1/3"></div>
+                  <div className="light green-light bg-green-500 w-1/3"></div>
                 </div>
               </div>
               <div className="box-2"></div>
@@ -36,15 +36,15 @@ export default class Pokedex extends Component {
             </div>
             </div>
             <div className="bottom-btns">
-            <i onClick={e => this.props.getPokemonById(e, this.props.selectedPokemon)}className="fas fa-caret-left back-btn text-orange-lightest"/>
+            <i onClick={e => this.props.getPokemonById(e, this.props.selectedPokemon)}className="fas fa-caret-left back-btn text-orange-300 hover:text-orange-400"/>
               <div className="name-bar shadow-md">
                 <p>{ this.props.error ? "???" : this.props.isFetchingPokemon ? "Gotta Catch Em All" :this.props.selectedPokemon.name}</p>
               </div>
-              <i onClick={e => this.props.getPokemonById(e, this.props.selectedPokemon)} className="fas fa-caret-right fwd-btn text-orange-lightest "/>
+              <i onClick={e => this.props.getPokemonById(e, this.props.selectedPokemon)} className="fas fa-caret-right fwd-btn text-orange-300 hover:text-orange-400"/>
             </div>
             <div className="search-bar bg-red-light block sm:hidden">
               <form onSubmit={this.props.getPokemon}>
-                  <input className="mt-6 bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-3/5 py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple"onChange={this.props.handleChanges} name="searchPoke" type="text" placeholder="Search..."/>
+                  <input className="mt-6 bg-gray-300 appearance-none border-2 border-gray-300 rounded w-3/5 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"onChange={this.props.handleChanges} name="searchPoke" type="text" placeholder="Search..."/>
               </form>
             </div>
         </div>
@@ -66,8 +66,8 @@ export default class Pokedex extends Component {
             <div className="box-2"></div>
             <div className="box-3"></div>
           </div>
-            <div className="stat-screen-container bg-red-light">
-              <div className="stat-screen text-green align-left ">
+            <div className="stat-screen-container bg-red-500">
+              <div className="stat-screen text-green-500 align-left ">
                 {this.props.isFetchingPokemon ? (<h2 className="mt-16">Searching...</h2> )
                 : this.props.error ? (<h2>This Pokemon does not exist, try searching again!</h2>)
                 :(<div>
@@ -88,12 +88,12 @@ export default class Pokedex extends Component {
               }
               </div>
             </div>
-            <div className="search-bar bg-red-light">
+            <div className="search-bar bg-red-500">
               <form onSubmit={this.props.getPokemon}>
-                  <input className="mt-6 bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-3/5 py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple"onChange={this.props.handleChanges} name="searchPoke" type="text" placeholder="Search..."/>
+                  <input className="mt-6 bg-gray-300 appearance-none border-2 border-gray-300 rounded w-3/5 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"onChange={this.props.handleChanges} name="searchPoke" type="text" placeholder="Search..."/>
               </form>
             </div>
-            <div className="random-btns-container bg-red-light">
+            <div className="random-btns-container bg-red-500">
             <div className="random-btns">
               <div className="blue-box"></div>
               <div className="blue-box"></div>
